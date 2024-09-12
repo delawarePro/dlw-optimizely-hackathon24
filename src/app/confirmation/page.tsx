1437dd3e094b92bd1e1f3b8ebbdfd307eccf0430
Lennert <lennertgavel@gmail.com>
2024-09-12 11:53:15 +0200

    fix link on conv page

--- src/app/confirmation/page.tsx
+++ src/app/confirmation/page.tsx
@@ -8,7 +8,7 @@ export default async function SearchPage() {
         <div className="container p-5 flex flex-col gap-5 flex-align-center justify-center grow">
             <h1 className="text-3xl">Congrats!! <br /> You c(b)aught yourself a pokemon!</h1>
             <h2 className="text-xl">Go catch a new one trainer!</h2>
-            <button type="button" className="pixl-button" href="/search">Let's go!!</button>
+            <a type="button" className="pixl-button" href="/search">Let's go!!</a>
         </div>
         </>
     );
