782e5ce22af9dd91a6202b3ef443b10901de5459
Bob Maes <bob.maes@delaware.pro>
2024-09-13 13:10:32 +0200

    Draft impl to return feature flags to vercel.

--- src/app/.well-known/vercel/flags/route.ts
+++ src/app/.well-known/vercel/flags/route.ts
@@ -1,20 +1,48 @@
-﻿import { NextResponse, type NextRequest } from 'next/server';
-import { verifyAccess, type ApiData } from '@vercel/flags';
+﻿
+
+import { NextResponse, type NextRequest } from 'next/server';
+import { verifyAccess, type ApiData, FlagDefinitionsType } from '@vercel/flags';
+import { OptimizelyConfig } from "@optimizely/optimizely-sdk";
+import { createInstance } from '@optimizely/optimizely-sdk/dist/optimizely.lite.min.js'
 
 export async function GET(request: NextRequest) {
     const access = await verifyAccess(request.headers.get('Authorization'));
     if (!access) return NextResponse.json(null, { status: 401 });
 
-    return NextResponse.json<ApiData>({
-        definitions: {
-            newFeature: {
-                description: 'Controls whether the new feature is visible',
-                origin: 'https://example.com/#new-feature',
-                options: [
-                    { value: false, label: 'Off' },
-                    { value: true, label: 'On' },
-                ],
-            },
-        },
+    // load datafile
+    const resp = await fetch('https://cdn.optimizely.com/datafiles/5MhHhxZ6yiwMtSeEtXQrp.json');
+    const datafile = await resp.json();
+    
+    const optimizelyClient : any  = createInstance({
+        datafile
     });
+
+    // fetch all feature flags from Optimizely
+    var enabledFeatures = optimizelyClient.getEnabledFeatures("user"); // TODO return user id from request
+
+    var definitions: FlagDefinitionsType = {};
+
+    enabledFeatures.forEach(feature => {
+        // const featureVariables = optimizelyClient.getFeatureVariables(feature);
+
+        const optlyConfig = optimizelyClient.getOptimizelyConfig();
+        const featureMap = optlyConfig.featuresMap[feature];
+        
+        const options = Object.keys(featureMap.variablesMap).map(key => ({
+            value: featureMap.variablesMap[key].value,
+            label: key,
+        }));
+        
+        definitions[feature] = {
+            description: `${feature}`,
+            origin: `https://origin/#${feature}`,
+            options: options,
+        };
+    });
+
+    const apiData: ApiData = {
+        definitions: definitions,
+    };
+
+    return NextResponse.json(apiData);
 }
