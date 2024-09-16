23671161db3a36ea81ba8558c004e15bc7b0202f
Lennert <lennertgavel@gmail.com>
2024-09-16 17:28:52 +0200

    lightbox

--- src/components/cms/element/WebEssentialRichTextMedia/index.tsx
+++ src/components/cms/element/WebEssentialRichTextMedia/index.tsx
@@ -13,31 +13,42 @@ import Image from "@/components/shared/cms-image";
 
 const { factory } = getServerContext();
 
-export const WebEssentialRichTextMediaElement : CmsComponent<WebEssentialRichTextMediaDataFragment> = ({ data, contentLink, children }) => {
+export const WebEssentialRichTextMediaElement: CmsComponent<WebEssentialRichTextMediaDataFragment> = ({ data, contentLink, children }) => {
+
+    const randomID = `lightbox-${Math.round(Math.random() * 1000 )}`;
 
     return (
         <CmsEditable cmsId={contentLink.key}>
-            <div className="relative">
-                <div className="mx-auto max-w-7xl lg:flex lg:justify-between lg:px-8 xl:justify-end">
-                    <div
-                        className="lg:flex lg:w-1/2 lg:shrink lg:grow-0 xl:absolute xl:inset-y-0 xl:right-1/2 xl:w-1/2">
-                        <div className="relative h-80 lg:-ml-8 lg:h-auto lg:w-full lg:grow xl:ml-0">
-                            <Image src={data?.Media} fill alt={""} className="object-cover"/>
-                        </div>
-                    </div>
-                    <div className="px-6 lg:contents">
-                        <div
-                            className="mx-auto max-w-2xl pb-24 pt-16 sm:pb-32 sm:pt-20 lg:ml-8 lg:mr-0 lg:w-full lg:max-w-lg lg:flex-none lg:pt-32 xl:w-1/2">
-                            <div className="mt-6 text-xl leading-8 text-gray-700">
-                                <RichText as="div"
-                                          className="mt-6 leading-8 text-gray-600"
-                                          cmsFieldName={"Content"}
-                                          text={data?.Content?.json} factory={factory}/>
+                <>
+                    <div className="relative">
+                        <div className="mx-auto max-w-7xl lg:flex lg:justify-between lg:px-8 xl:justify-end">
+                            <div className="lg:flex lg:w-1/2 lg:shrink lg:grow-0 xl:absolute xl:inset-y-0 xl:right-1/2 xl:w-1/2">
+                                <button popovertarget={randomID} className="relative h-80 lg:-ml-8 lg:h-auto lg:w-full lg:grow xl:ml-0">
+                                    <Image src={data?.Media} fill alt={""} className="object-cover" />
+                                </button>
+                            </div>
+                            <div className="px-6 lg:contents">
+                                <div
+                                    className="mx-auto max-w-2xl pb-24 pt-16 sm:pb-32 sm:pt-20 lg:ml-8 lg:mr-0 lg:w-full lg:max-w-lg lg:flex-none lg:pt-32 xl:w-1/2">
+                                    <div className="mt-6 text-xl leading-8 text-gray-700">
+                                        <RichText as="div"
+                                            className="mt-6 leading-8 text-gray-600"
+                                            cmsFieldName={"Content"}
+                                            text={data?.Content?.json} factory={factory} />
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
+
+
+                    <div popover="true" className="show-lightbox" id={randomID}>
+                        <div className="lightbox-container">
+                            <Image src={data?.Media} fill alt={""} className="object-cover" />
+                            <button popovertarget={randomID} popovertargetaction="hide">Close</button>
+                        </div>
+                    </div>
+                </>
         </CmsEditable>
     )
 }
