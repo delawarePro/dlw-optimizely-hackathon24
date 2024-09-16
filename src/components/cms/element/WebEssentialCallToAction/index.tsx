0d65afea931c8e8f2a14b490447ebd0262087c0b
Bob Maes <bob.maes@delaware.pro>
2024-09-17 00:33:05 +0200

    Minor hack to tweak homepage.

--- src/components/cms/element/WebEssentialCallToAction/index.tsx
+++ src/components/cms/element/WebEssentialCallToAction/index.tsx
@@ -10,7 +10,7 @@ export const WebEssentialCallToActionElement : CmsComponent<WebEssentialCallToAc
     const url = (data?.Link as LinkDataFragment)?.default || "#";
     
     return (
-        <div className="m-10">
+        <div className="m-20">
             <a href={url}
                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                 {data.Label}
