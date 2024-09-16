873fcea12c3a71360acff8e2e7f771cbea5dab01
Lennert <lennertgavel@gmail.com>
2024-09-16 10:50:22 +0200

    read order number in queryparam on confirmation

--- src/app/confirmation/page.tsx
+++ src/app/confirmation/page.tsx
@@ -1,17 +1,25 @@
 ﻿'use client';
 
-import React from "react";
+import React from 'react'
+import { useSearchParams } from 'next/navigation'
+
 import { Sause } from '../search/components/sause';
 
 export default async function SearchPage() {
+    const searchParams = useSearchParams();
+    const orderID = searchParams.get('orderNr');
+
     return (
         <>
         <div className="container p-5 flex flex-col gap-5 flex-align-center justify-start grow">
             <Sause useplane={false} />
-
             <h1 className="text-3xl">Congrats!! <br /> You c(b)aught yourself a pokemon!</h1>
-            <h2 className="text-xl">Go catch a new one trainer!</h2>
-            <a type="button" className="pixl-button" href="/search">Let's go!!</a>
+            <h2 className="text-xl">Your order id:<br />
+                <span className="text-blue-600">{orderID}</span>
+            </h2>
+
+            <p className="text-xl">Go catch a new one trainer!</p>
+            <a type="button" className="pixl-button pixl-button--lg" href="/search">Let's go!!</a>
         </div>
         </>
     );
