211220da3eadfa9f3fe176c47373840d0f9566a0
Lennert <lennertgavel@gmail.com>
2024-09-17 09:10:44 +0200

    slicing rich text

--- src/components/cms/element/WebEssentialRichTextMedia/index.tsx
+++ src/components/cms/element/WebEssentialRichTextMedia/index.tsx
@@ -17,18 +17,18 @@ export const WebEssentialRichTextMediaElement: CmsComponent<WebEssentialRichText
     return (
         <CmsEditable cmsId={contentLink.key}>
             <>
-                <div className="relative">
+                <div className="relative c-rich-text">
                     <div className="mx-auto max-w-7xl lg:flex lg:justify-between lg:px-8 xl:justify-end">
-                        <div className="lg:flex lg:w-1/2 lg:shrink lg:grow-0 xl:absolute xl:inset-y-0 xl:right-1/2 xl:w-1/2">
-                            <button popoverTarget={randomID} className="relative h-80 lg:-ml-8 lg:h-auto lg:w-full lg:grow xl:ml-0">
+                        <div className="lg:flex lg:w-1/2 lg:shrink lg:grow-0 xl:absolute xl:inset-y-0 xl:right-1/2 xl:w-1/2 c-rich-text__img-container">
+                            <button popoverTarget={randomID} className="relative h-80 lg:-ml-8 lg:h-auto lg:w-full lg:grow xl:ml-0 lightbox-open-button">
                                 <Image src={data?.Media} fill alt={""} className="object-cover" />
                             </button>
                         </div>
                         <div className="px-6 lg:contents">
                             <div
                                 className="mx-auto max-w-2xl pb-24 pt-16 sm:pb-32 sm:pt-20 lg:ml-8 lg:mr-0 lg:w-full lg:max-w-lg lg:flex-none lg:pt-32 xl:w-1/2">
-                                <div className="mt-6 text-xl leading-8 text-gray-700">
-                                    <RichText className="mt-6 leading-8 text-gray-600"
+                                <div className="mt-6 text-xl leading-8">
+                                    <RichText className="mt-6 leading-8"
                                               text={data?.Content?.json} />
                                 </div>
                             </div>
@@ -39,8 +39,8 @@ export const WebEssentialRichTextMediaElement: CmsComponent<WebEssentialRichText
 
                 <div popover="" className="show-lightbox" id={randomID}>
                     <div className="lightbox-container">
-                        <Image src={data?.Media} fill alt={""} className="object-cover" />
-                        <button popoverTarget={randomID} popoverTargetAction="hide">Close</button>
+                        <Image src={data?.Media} fill alt={""} className="object-contain" />
+                        <button popoverTarget={randomID} popoverTargetAction="hide" className="pxl-font">x</button>
                     </div>
                 </div>
             </>
